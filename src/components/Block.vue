f331824d3f9f12fc161f204f8a207bb9e77d4423
NNEUID <nneuid@gmail.com>
2022-12-20 09:08:54 +0000

    component lifecycle hooks in vue 3

---
+++ src/components/Block.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="block" v-if="showBlock">
+        Click Me
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Block',
+    props: ['delay'],
+    data() {
+      return {
+        showBlock: false
+      }
+    },
+    mounted() {
+      setTimeout(()=> {
+        this.showBlock = true
+      },this.delay)
+    },
+    updated() {
+      console.log('component updated');
+    },
+    upmounted() {
+      console.log('component unmounted');
+    }
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
